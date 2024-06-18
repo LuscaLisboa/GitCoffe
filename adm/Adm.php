1a60e6f8af332c6de8a95a0bd3af53d7538faf37
hprodeliki <hpprode@gmail.com>
2024-06-18 20:35:18 -0300

    Editar produto css

--- adm/Adm.php
+++ adm/Adm.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Painel Admin</title>
-    <link rel="stylesheet" href="css/Adm.css">
+    <link rel="stylesheet" href="../css/Adm.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
