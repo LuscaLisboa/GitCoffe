d16bbc188d8d524149532889832865ff45793094
MarlonVolff <marlonvolffsilva@gmail.com>
2024-06-18 22:44:51 -0300

    add e erros

--- Carrinho.php
+++ Carrinho.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrinho de Compras</title>
-    <link rel="stylesheet" href="css/Carrinho.css">
     <link rel="stylesheet" href="css/Adm.css">
     <link rel="stylesheet" href="css/cabecalho.css">
 </head>
